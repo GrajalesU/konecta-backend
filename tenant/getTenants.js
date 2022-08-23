5a5ac0d4253876199e96434a5ee7f96b6cd25b7d
JulianaCadavid <juliana.cadavidr@udea.edu.co>
2022-08-23 16:26:23 -0500

    Get tenants completed

--- tenant/getTenants.js
+++ tenant/getTenants.js
@@ -1,21 +1,26 @@
 import mysql from "../database.js";
 
-export async function handler(event) {
-  let results = await mysql.query(`
-  SELECT * FROM tenant 
-    JOIN company
-    ON tenant.id = company.id
-    JOIN representative
-    ON representative_id = representative.id
-  WHERE its_active = 1
-  
-  `);
-  await mysql.end();
-
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      results,
-    }),
-  };
-}
+module.exports.getTenants = (event, context, callback) => {
+  context.callbackWaitsForEmptyEventLoop = false;
+  const sql = `SELECT * FROM konecta.tenant
+      JOIN company
+      ON tenant.id = company.id
+      JOIN representative
+      ON representative_id = representative.id
+    WHERE its_active = 1`;
+  mysql.query(sql, (error, rows) => {
+    if (error) {
+      callback({
+        statusCode: 500,
+        body: JSON.stringify(error),
+      });
+    } else {
+      callback(null, {
+        statusCode: 200,
+        body: JSON.stringify({
+          tenant: rows,
+        }),
+      });
+    }
+  });
+};
